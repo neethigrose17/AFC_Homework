1fb7733d86d2de9ffd46175a3eba3af45007f2c5
Neethi Grose <neethi.grose.mil@swf.army.mil>
2023-05-18 13:35:40 -0500

    DiceRoll complete

--- DiceRoll/src/App.jsx
+++ DiceRoll/src/App.jsx
@@ -1,25 +1,35 @@
 import Dice from "./components/Dice";
-import {getNumber} from "./helper";
+import {getNumber} from "./helper"; // import random number function
 import {useState, useEffect} from "react";
 
 const App = () => {
-  const [diceOne, setDiceOne] = useState(getNumber());
+  const [diceOne, setDiceOne] = useState(getNumber()); // start state of dice
   const [diceTwo, setDiceTwo] = useState(getNumber());
-  let diceOneVal = diceOne + 1;
+  let diceOneVal = diceOne + 1; // numbers to add for sum
   let diceTwoVal = diceTwo + 1;
   let sum = diceOneVal + diceTwoVal;
   const handleClick = () => {
-    setDiceOne(getNumber());
+    setDiceOne(getNumber()); // when you click the button, reset state of dice
     setDiceTwo(getNumber());
-    diceOneVal = diceOne + 1;
+    diceOneVal = diceOne + 1; // reset variables for value of dice
     diceTwoVal = diceTwo + 1;
   }
+
+  useEffect( () => {
+    // the Dice Roll app works fine without this useEffect so I am not sure why it's needed
+  }, [diceOne, diceTwo])
+
   return (
     <>
-      <h1>I am the App component</h1>
-      <div id="dice1"><Dice number={diceOne}/></div>
-      <div id="dice2"><Dice number={diceTwo}/></div>
-      <h2>Roll equals {sum}</h2>
+      <h1 id="title">Dice Roller App</h1>
+      {/* pass random numbers as props to dice component */}
+      <div id="dice">
+        {/* the div IDs allow for CSS styling */}
+        <div id="dice1"><Dice number={diceOne}/></div>
+        <div id="dice2"><Dice number={diceTwo}/></div>
+      </div>
+      <h2 id="sum">Roll equals {sum}</h2>
+      {/* change state of dice when the button is clicked */}
       <button onClick={handleClick}>Roll the Dice!</button>
     </>
   )
--- DiceRoll/src/components/Dice.jsx
+++ DiceRoll/src/components/Dice.jsx
@@ -1,9 +1,11 @@
-import {numWords} from "../helper";
+import {numWords} from "../helper"; // import array of number words
 
 const Dice = (props) => {
+  // use number props from parent App component to determine which dice are displayed
   const {number} = props;
     return (
       <>
+        {/* number prop is used as index from numWords array */}
         <h1><i className={`fa-solid fa-dice-${numWords[number]}`}></i></h1>
       </>
     )
