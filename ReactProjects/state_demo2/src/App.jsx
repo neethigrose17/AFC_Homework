e56598821669c53127ae86f7afaaf9bfdbd43f32
Neethi Grose <neethi.grose.mil@swf.army.mil>
2023-05-18 11:26:21 -0500

    updated state_demo2

--- ReactProjects/state_demo2/src/App.jsx
+++ ReactProjects/state_demo2/src/App.jsx
@@ -1,8 +1,17 @@
 
 const App = () => {
+  // let hockeyStuff = ["sticks", "puck", "mask"]
+  // let hockeyArray = hockeyStuff.map((el, i) => {
+  //   return <h1 id="hockey" key={i}><i className={`fas fa-hockey-${el}`}></i></h1>
+  // })
   return (
     <>
       <h1>App Component</h1>
+      {/* <h1><i className="fa-light fa-hockey-sticks"></i></h1> */}
+      {/* {hockeyArray} */}
+      <i className="fa-solid fa-hockey-puck"></i>
     </>
   )
 }
+
+export default App;
