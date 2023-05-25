752407900b2139c22c08f5a1a677e476b0defb12
Neethi Grose <neethi.grose.mil@swf.army.mil>
2023-05-25 11:42:26 -0500

    CRUD done on todos

--- ReactProjects/simple_todos/src/App.jsx
+++ ReactProjects/simple_todos/src/App.jsx
@@ -6,7 +6,6 @@ const endpoint = "https://accbucketlists.herokuapp.com/user/piyush/bucket";
 // CRUD
 
 // Update
-// Delete
 
 const App = () => {
   // set our states
@@ -18,16 +17,45 @@ const App = () => {
     console.log(inputValue);
   }
 
+  const handleKeyDown = (event) => {
+    // user cannot submit using enter key
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+  }
+
+  // Delete
+  const handleDelete = (event) => {
+    event.stopPropagation(); // prevents bubbling so that when you delete it doesn't try to update
+    // data attributes
+    // we need id from selected todo
+    // compare current todos with selected id
+    // remove todo with selected id
+    let requestedID = event.target.parentElement.dataset.id
+    console.log(requestedID)
+    console.log(event.target.parentElement.dataset.id)
+    // todos.find(el => {
+    //   return requestedID === el.id
+    // })
+    let newTodos = todos.filter(el => {
+      console.log(typeof requestedID);
+      return el.id + "" !== requestedID
+    })
+    setTodos(newTodos)
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Form has been submitted");
-    let todoObj = {
-      id: Date.now(),
-      description: inputValue,
-      isComplete: false
+    if (inputValue.trim()) {
+      let todoObj = {
+        id: Date.now(),
+        description: inputValue,
+        isComplete: false
+      }
+      let items = [...todos, todoObj];
+      setTodos(items);
     }
-    let items = [...todos, todoObj];
-    setTodos(items)
     // reset input text box
     setInputValue("")
   }
@@ -46,10 +74,33 @@ const App = () => {
     })
   }, [])
 
+    // Update
+    const handleUpdate = () => {
+      let copyOfTodos = [...todos]
+      // get id from selected todo
+      let requestedID = event.target.dataset.id
+      let newTodo = todos.find(el => {
+        console.log(typeof requestedID);
+        return requestedID === el.id + ""
+      })
+      newTodo.isComplete = !newTodo.isComplete
+      setTodos(copyOfTodos) // why does this work?
+    }
+
   // Create
 
   let todosArray = todos.map((el, i) => {
-    return <li key={el.id}>{el.description}</li>
+    return (
+        <li
+          onClick={handleUpdate}
+          key={el.id}
+          data-id={el.id}
+          className={el.isComplete ? "completed" : ""}
+        >
+          <button onClick={handleDelete}>X</button>
+          {el.description}
+        </li>
+    )
   })
 
   return (
@@ -63,6 +114,7 @@ const App = () => {
           value={inputValue}
           placeholder="Enter new todo here..."
           onChange={handleChange}
+          onKeyDown={handleKeyDown}
         />
         <button type="submit">Submit</button>
       </form>
